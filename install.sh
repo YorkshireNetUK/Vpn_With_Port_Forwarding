c088af24b9825c5191ea9a90812bb283a8f17bca
YorkshireNetUK <132102968+YorkshireNetUK@users.noreply.github.com>
2025-02-20 20:37:28 +0000

    Update and rename openvpn-install.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #
-# https://github.com/Nyr/openvpn-install
-#
-# Copyright (c) 2013 Nyr. Released under the MIT License.
-
+# https://github.com/Nyr/install.sh
 
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
